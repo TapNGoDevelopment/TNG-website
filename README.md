0061aded821d3bcd67a048e5f5d965f20a92cba6
Damini Patel <89243388+TapNGoDevelopment@users.noreply.github.com>
2022-05-03 13:35:46 +1000

    Digital

--- README.md
+++
@@ -1,1 +0,0 @@
-# Stripe-Node-Project
--- constants.js
+++
@@ -1,9 +0,0 @@
-module.exports = Object.freeze({
-    SMTP_EMAIL: "damini8patel@gmail.com",
-    SMTP_PASSWORD:"Welcome@Damini123",
-    SMTP_SERVER:"gmail",
-    SMTP_FROM_EMAIL:"noreply@tapngo.com.au",
-    SMTP_FROM_NAME:"TAP N GO",
-    TO_EMAIL:"daminipatel19958@gmail.com"
-
-});
