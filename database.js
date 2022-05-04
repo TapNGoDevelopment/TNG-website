5b58b0486ee71b7de1f3394cfcac02357b9ce2de
Damini Patel <89243388+TapNGoDevelopment@users.noreply.github.com>
2022-05-04 12:33:24 +1000

    comman changes done

--- database.js
+++ database.js
@@ -1,9 +1,15 @@
 var mysql = require('mysql');
+const host = process.env.RDS_HOSTNAME || 'localhost';
+const user = process.env.RDS_USERNAME || 'root'; 
+const password = process.env.RDS_PASSWORD || ''; 
+const port = process.env.RDS_PORT || '';
+const database = process.env.RDS_DB_NAME || 'node'; 
 var conn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'node'
+    host     : host,
+    user     : user,
+    password : password,
+    port     : port,
+    database : database
 });
 conn.connect(function(err) {
     if (err) throw err;
